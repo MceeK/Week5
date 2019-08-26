148e223eab717f4f04a49f2dc020d61e4e3235a5
MceeL <cho0019@jmss.vic.edu.au>
2019-08-26 17:19:31 +1000

    Change send methods + cleanup

--- server.js
+++ server.js
@@ -18,7 +18,6 @@ app.set('view engine', 'html');
 app.set('port', 8888);
 
 app.post('/newTask', function(req,res){
-    // console.log(req.body.taskName);
     let taskName = req.body.taskName;
     let taskDate = req.body.taskDate;
     let taskDesc = req.body.taskDesc;
@@ -29,32 +28,19 @@ app.post('/newTask', function(req,res){
         taskDesc: taskDesc
     };
     db.push(rec);
-    // res.send(listAll());
     res.render('listAll.html', {data: db});
 });
 
 app.get('/', function(req,res){
-    res.render('index.html');
+    res.sendFile(__dirname + '/views/index.html');
 });
 
 app.get('/newTask', function(req,res){
-    res.render('newTask.html');
+    res.sendFile(__dirname + '/views/newTask.html');
 });
 
 app.get('/listAll', function(req,res){
-    // res.send(listAll());
     res.render('listAll.html', {data: db});
 });
 
 app.listen(app.get('port'));
-
-function listAll() {
-    let toOut = '<table style="width:100%"><tr><th>Task Name</th><th>Task Date</th><th>Task Description</th></tr>';
-    for (let i = 0; i < db.length; i++) {
-        toOut += '<tr><td>' + db[i].taskName + '</td><td>' + db[i].taskDate + '</td><td>' + db[i].taskDesc + '</td></tr>';
-        if (i == db.length - 1) {
-            toOut += '</table>';
-        };
-    }
-    return toOut;
-};
